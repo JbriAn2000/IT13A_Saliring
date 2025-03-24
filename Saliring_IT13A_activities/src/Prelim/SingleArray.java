56d93cb6b3f3b8fcb054d4bc5d0c0a44313d101a
JbriAn2000 <saliring0531@gmail.com>
2025-03-25 07:45:34 +0800

    Add files via upload

---
+++ Saliring_IT13A_activities/src/Prelim/SingleArray.java
@@ -0,0 +1,14 @@
+
+package midterm;
+
+public class SingleArray {
+    public static void main(String[] args){
+        char[] letters = {'A','B','C','D','E','F','G'};
+        
+       for(char x = 0; x < letters.length; x++) {
+           System.out.print(letters [x] + " ");
+       }
+       System.out.println();
+    }
+    
+}
