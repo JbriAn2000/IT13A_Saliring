e29bf34c656ac8aa1ad5fae693a2fd6301854af9
JbriAn2000 <saliring0531@gmail.com>
2025-04-08 16:58:22 +0800

    Add files via upload

---
+++ Saliring_IT13A_activities/src/Prelim/CreateFile.java
@@ -0,0 +1,57 @@
+
+package practice;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args){
+        try {
+            File myFile = new File("D:\\Practice.txt");
+            if(myFile.createNewFile()){
+                System.out.println("File created:" + myFile.getName());
+            }else{
+                System.out.println("File already exist.");
+            }
+            int key =7;
+            String m = "I LOVE YOU";
+            String m1 = "Gwapo ko";
+            String m2 = "Boutan kaayo ko";
+            String m3 = encryptMessage(m , key);
+            String m4 = encryptMessage(m1 , key);
+             String m5 = encryptMessage2(m2 , key);
+             FileWriter writer = new FileWriter(myFile);
+             writer.write("Original Message1:" + m + "\n");
+             writer.write("Encrypted Message1:" + m3 + "\n\n");
+              writer.write("Original Message2:" + m1 + "\n");
+             writer.write("Encrypted Message2:" + m4 + "\n\n");
+              writer.write("Original Message3:" + m2 + "\n");
+             writer.write("Encrypted Message3:" + m5 + "\n\n");
+             writer.close();
+             System.out.println("Successfully wrote to the file");
+             
+        }catch (IOException e){
+            System.out.println("An error occured.");
+            e.printStackTrace();
+            
+        }
+   }
+    public static String encryptMessage(String message, int key){
+        char[] chars = message.toCharArray();
+        for(int i = 0; i < chars.length; i++){
+            chars[i] = (char)(chars[i] + key);
+        }
+        return new String(chars);      
+    }
+    public static String encryptMessage2(String message, int key){
+        char[] chars = message.toCharArray();
+        for(int i = 0; i < chars.length; i++){
+            chars[i] = (char)(chars[i] + key);
+      }
+        return new String(chars);
+    }
+}
+
+    
+
+
